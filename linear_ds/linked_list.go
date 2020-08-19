cf203aef8435a9f46c3c5c95dcc350598ef78e9b
iliassaek <iliassaek.dev@gmail.com>
2020-08-19 12:52:24 +0000

    AddToHead method

--- linear_ds/linked_list.go
+++ linear_ds/linked_list.go
@@ -4,16 +4,39 @@ import(
 	"fmt"
 )
 
+// create basic node
 type Node struct{
 	property int
 	nextNode *Node
 }
 
+// create the head node
+type LinkedList struct{
+	headNode *Node
+}
+
+func (linkedList *LinkedList)AddToHead(property int){
+
+	var node *Node
+	node = &Node{property: property,nextNode: nil}
+	
+	if linkedList.headNode != nil {
+		node.nextNode= linkedList.headNode
+	}
+	
+	//in all cases
+	linkedList.headNode= node
+}
+
 
 func main() {
 
-	var node Node
-	node = Node{property: 1}
-	fmt.Println(node.property)	
+	var linkedList LinkedList
+	linkedList.AddToHead(1)
+	linkedList.AddToHead(2)
+
+	//should print 2
+	fmt.Println(linkedList.headNode.property)
+
 }
 
