76b4efc002fb0544817c690b57a9214020261db8
iliassaek <iliassaek.dev@gmail.com>
2020-08-20 11:03:15 +0000

    union of sets

--- linear_ds/set.go
+++ linear_ds/set.go
@@ -41,6 +41,22 @@ func (set *Set) Intersect(anotherSet *Set) *Set{
 	return intersectSet
 }
 
+func (set *Set) Union(anotherSet *Set) *Set{
+	var unionSet= &Set{}
+	unionSet.New()
+	
+	var value int
+	for value,_ = range set.integerMap{
+		unionSet.AddElement(value)
+	}
+
+	for value,_ = range anotherSet.integerMap{
+		unionSet.AddElement(value)
+	}
+
+	return unionSet
+}
+
 func main(){
 
 	var set = &Set{}
@@ -53,7 +69,7 @@ func main(){
 	anotherSet.AddElement(1)
 	anotherSet.AddElement(2)
 
-	var interSet = set.Intersect(anotherSet)	
-	//should return &{map[2:true]}
-	fmt.Println(interSet)
+	var unionSet = set.Union(anotherSet)	
+	//should return &{map[1:true 2:true 3:true]}
+	fmt.Println(unionSet)
 }
